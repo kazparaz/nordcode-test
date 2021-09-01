1b8f8fe24345fd4189d5b6871072d5c5aeb3cf29
Kasparas <kasparas@searchnode.com>
2021-09-01 14:04:34 +0300

    Render real data in WeekForecast

--- src/components/WeekForecast/WeekForecast.tsx
+++ src/components/WeekForecast/WeekForecast.tsx
@@ -1,41 +1,25 @@
+import { useHookstate } from '@hookstate/core'
+import { globalState } from '../../state'
 import commonStyles from '../../styles/common.module.scss'
 import { Flex } from '../Flex'
 import styles from './WeekForecast.module.scss'
 
-const forecast = [
-  {
-    day: 'FRI',
-    temperature: [8, 4],
-  },
-  {
-    day: 'SAT',
-    temperature: [7, 2],
-  },
-  {
-    day: 'SUN',
-    temperature: [4, 3],
-  },
-  {
-    day: 'MON',
-    temperature: [7, 4],
-  },
-  {
-    day: 'TUE',
-    temperature: [8, 8],
-  },
-  {
-    day: 'WED',
-    temperature: [11, 5],
-  },
-  {
-    day: 'THU',
-    temperature: [7, 6],
-  },
-] as const
-
 export const WeekForecast = (): JSX.Element => {
-  const highestTemperature = forecast.reduce(
-    (acc, item) => Math.max(acc, ...item.temperature),
+  const weather = useHookstate(globalState.weather)
+  const weatherData =
+    !weather.promised &&
+    typeof weather.value === 'object' &&
+    'current' in weather.value
+      ? weather.value
+      : undefined
+  const dailyWeather = weatherData?.daily.slice(1).map((item) => ({
+    ...item,
+    dayName: new Date(item.dt * 1000).toLocaleDateString('en', {
+      weekday: 'short',
+    }),
+  }))
+  const highestTemperature = dailyWeather?.reduce(
+    (acc, item) => Math.max(acc, item.temp.day, item.temp.night),
     0
   )
   return (
@@ -43,34 +27,40 @@ export const WeekForecast = (): JSX.Element => {
       <h2 className={commonStyles.sectionTitle}>Week Forecast</h2>
 
       <Flex className={styles.itemsWrapper}>
-        {forecast.map((item) => (
-          <div key={item.day} className={styles.item}>
-            <h3 className={styles.itemDay}>{item.day}</h3>
+        {highestTemperature !== undefined &&
+          dailyWeather?.map((item) => (
+            <div key={item.dt} className={styles.item}>
+              <h3 className={styles.itemDay}>{item.dayName}</h3>
 
-            <p className={styles.itemTemperature}>
-              {item.temperature[0]} {item.temperature[1]}
-            </p>
+              <p className={styles.itemTemperatures}>
+                <span className={styles.itemTempDay}>
+                  {item.temp.day.toFixed()}
+                </span>
+                <span className={styles.itemTempNight}>
+                  {item.temp.night.toFixed()}
+                </span>
+              </p>
 
-            <Flex
-              className={styles.itemBarsWrapper}
-              alignItems='flex-end'
-              gap='1px'
-            >
-              {[
-                { className: styles.itemBarDay, value: item.temperature[0] },
-                { className: styles.itemBarNight, value: item.temperature[1] },
-              ].map((bar) => (
-                <div
-                  key={bar.className}
-                  className={bar.className}
-                  style={{
-                    height: `${(bar.value / highestTemperature) * 100}%`,
-                  }}
-                />
-              ))}
-            </Flex>
-          </div>
-        ))}
+              <Flex
+                className={styles.itemBarsWrapper}
+                alignItems='flex-end'
+                gap='1px'
+              >
+                {[
+                  { className: styles.itemBarDay, value: item.temp.day },
+                  { className: styles.itemBarNight, value: item.temp.night },
+                ].map((bar) => (
+                  <div
+                    key={bar.className}
+                    className={bar.className}
+                    style={{
+                      height: `${(bar.value / highestTemperature) * 100}%`,
+                    }}
+                  />
+                ))}
+              </Flex>
+            </div>
+          ))}
       </Flex>
     </section>
   )
