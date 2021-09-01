fa389b894f737861ae0796662f40125ed9f5d83a
Kasparas <kasparas@searchnode.com>
2021-09-01 13:44:45 +0300

    Render real data in DayForecast

--- src/components/DayForecast/DayForecast.tsx
+++ src/components/DayForecast/DayForecast.tsx
@@ -6,13 +6,31 @@ import styles from './DayForecast.module.scss'
 
 export const DayForecast = (): JSX.Element => {
   const city = useHookstate(globalState.city)
+  const weather = useHookstate(globalState.weather)
+  const weatherData =
+    !weather.promised &&
+    typeof weather.value === 'object' &&
+    'current' in weather.value
+      ? weather.value
+      : undefined
 
   return (
     <Flex as='section' className={commonStyles.flexGrow} flexDirection='column'>
       <h2 className={commonStyles.sectionTitle}>
         <Flex justifyContent='space-between'>
           <span>Day Forecast</span>
-          <span>8 2</span>
+          <span>
+            <span className={styles.tempDay}>
+              {weatherData?.daily[0]?.temp.day
+                ? Math.round(weatherData?.daily[0]?.temp.day)
+                : undefined}
+            </span>
+            <span className={styles.tempNight}>
+              {weatherData?.daily[0]?.temp.night
+                ? Math.round(weatherData?.daily[0]?.temp.night)
+                : undefined}
+            </span>
+          </span>
         </Flex>
       </h2>
 
@@ -22,9 +40,13 @@ export const DayForecast = (): JSX.Element => {
         alignItems='center'
         justifyContent='center'
       >
-        <div className={styles.temperature}>6°</div>
+        <div className={styles.tempCurrent}>
+          {weatherData?.current.temp
+            ? `${Math.round(weatherData?.current.temp)}°`
+            : undefined}
+        </div>
         <div className={styles.city}>{city.value.name}</div>
-        <div>Cloudy</div>
+        <div>{weatherData?.current.weather[0]?.main}</div>
       </Flex>
     </Flex>
   )
